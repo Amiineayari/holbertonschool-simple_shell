b477af5bb09983bfb9d0ef24f8c44f06312a0cea
Amiineayari <6485@holbertonstudents.com>
2023-04-28 15:58:52 -0700

    simple shell

--- main.h
+++ main.h
@@ -5,12 +5,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
 int shell(char *readline);
 char *get_the_line(void);
 char **parse_the_line(char *readline);
 void sigintHandler(__attribute__((unused)) int sig_num);
 int execute_the_line(char **command);
 int main(void);
-#endif
-  
+#endi
